57f53a8fb383540872e95831a150f544541b9e27
Pawlox4E <pawlonix@interia.eu>
2024-05-30 03:10:38 +0200

    Menu - zrobione, ale do malych poprawek #do zrobienia uruchamianie gry z menu

--- Gra/menu.py
+++ Gra/menu.py
@@ -1,14 +1,49 @@
 import random
 import pygame
 import sys
+import math
+from game_main import main_game
+
+class Button:
+    def __init__(self, x, y, width, height, text, font, font_color, background_image):
+        self.rect = pygame.Rect(x-width/2, y-height/2, width, height)
+        self.text = text
+        self.font = font
+        self.font_color = font_color
+        self.background_image = pygame.transform.scale(background_image, (width, height))
+
+    def draw(self, surface):
+        surface.blit(self.background_image, (self.rect.x, self.rect.y))
+        text_surface = self.font.render(self.text, True, self.font_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+
+    def check_click(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
 
 pygame.init()
 
-WINDOW_WIDTH = 1920
-WINDOW_HEIGHT = 1080
-FPS = 60
 
-WHITE = (255, 255, 255)
+#odczyt ustawien, mozna zaimportowac do np main_game
+
+settings_file_path = "data/settings"
+def read_settings(file_path, key):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith(key):
+                _, value = line.split('=')
+                return int(value.strip())
+    return None
+
+
+WINDOW_WIDTH = read_settings(settings_file_path, "WIDTH")
+WINDOW_HEIGHT = read_settings(settings_file_path, "HEIGHT")
+FPS = read_settings(settings_file_path, "FPS")
+
+#font settings
+font_size = int(math.sqrt(WINDOW_WIDTH * WINDOW_HEIGHT) * 0.02)
+fontColor = (255, 255, 255)
+font = pygame.font.Font("fonts/GloriousChristmas-BLWWB.ttf", font_size)
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Our Game(menu)")
@@ -20,9 +55,14 @@ background_rect = background_image.get_rect()
 background_rect.right = WINDOW_WIDTH
 
 button_background = pygame.image.load("images/menu/button_background1.png").convert_alpha()
-button_background = pygame.transform.scale(button_background, (600, 100))
 
-my_font = pygame.font.Font("fonts/GloriousChristmas-BLWWB.ttf", 40)
+#latwiej by bylo w liscie - i obslugiwac eventy / rysowanie przez petle
+#ale trzeba ogarnac dodanie funkcji np w kostruktorze przycisku  - przez 1 petle na liscie nie obsluzy sie klikniec
+buttons = []
+startButton = Button(WINDOW_WIDTH // 2, 275, 400, 100, "Start", font, fontColor, button_background)
+settingButton = Button(WINDOW_WIDTH // 2, 375, 400, 100, "Settings", font, fontColor, button_background)
+dummyButton = Button(WINDOW_WIDTH // 2, 475, 400, 100, "DummyBUtton", font, fontColor, button_background)
+quitButton = Button(WINDOW_WIDTH // 2, 575, 400, 100, "Quit", font, fontColor, button_background)
 
 def draw_text(surface, text, font, color, x, y):
     text_surface = font.render(text, True, color)
@@ -31,7 +71,6 @@ def draw_text(surface, text, font, color, x, y):
     surface.blit(text_surface, text_rect)
 
 
-# Funkcja główna menu
 def main_menu():
     running = True
     move_left = True
@@ -50,45 +89,66 @@ def main_menu():
 
         screen.blit(background_image, background_rect)
 
-        button_rect = button_background.get_rect()
-        button_rect.center = (WINDOW_WIDTH // 2, 275)
-        screen.blit(button_background, button_rect)
+        #pozyczyłem licznik fps
+        fps = font.render(str(int(clock.get_fps())), True, (255, 0, 0))
+        screen.blit(fps, (WINDOW_WIDTH - 60, 0))
 
-        button_rect.center = (WINDOW_WIDTH // 2, 375)
-        screen.blit(button_background, button_rect)
-
-        button_rect.center = (WINDOW_WIDTH // 2, 475)
-        screen.blit(button_background, button_rect)
-
-        button_rect.center = (WINDOW_WIDTH // 2, 575)
-        screen.blit(button_background, button_rect)
-
-        # Rysowanie tekstu na przyciskach
-        draw_text(screen, "START", my_font, WHITE, WINDOW_WIDTH // 2, 255)
-        draw_text(screen, "SETTTINGS", my_font, WHITE, WINDOW_WIDTH // 2, 355)
-        draw_text(screen, "DUMMYBUTTON", my_font, WHITE, WINDOW_WIDTH // 2, 455)
-        draw_text(screen, "EXIT", my_font, WHITE, WINDOW_WIDTH // 2, 555)
+        # button_rect = button_background.get_rect()
+        # button_rect.center = (WINDOW_WIDTH // 2, 275)
+        # screen.blit(button_background, button_rect)
+        #
+        # button_rect.center = (WINDOW_WIDTH // 2, 375)
+        # screen.blit(button_background, button_rect)
+        #
+        # button_rect.center = (WINDOW_WIDTH // 2, 475)
+        # screen.blit(button_background, button_rect)
+        #
+        # button_rect.center = (WINDOW_WIDTH // 2, 575)
+        # screen.blit(button_background, button_rect)
+        #
+        # # Rysowanie tekstu na przyciskach
+        # draw_text(screen, "START", font, fontColor, WINDOW_WIDTH // 2, 255)
+        # draw_text(screen, "SETTTINGS", font, fontColor, WINDOW_WIDTH // 2, 355)
+        # draw_text(screen, "DUMMYBUTTON", font, fontColor, WINDOW_WIDTH // 2, 455)
+        # draw_text(screen, "EXIT", font, fontColor, WINDOW_WIDTH // 2, 555)
+        startButton.draw(screen)
+        settingButton.draw(screen)
+        dummyButton.draw(screen)
+        quitButton.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            #hujowe - do przerobki odczytwyanie klikniecia
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                #print(mouse_pos)
-                if 685 <= mouse_pos[0] <= 1230 and 245 <= mouse_pos[1] <= 305:
-                    print("START")
-                if 685 <= mouse_pos[0] <= 1230 and 345 <= mouse_pos[1] <= 405:
+                mousePos = pygame.mouse.get_pos()
+                if startButton.check_click(mousePos):
+                    main_game()
+                if settingButton.check_click(mousePos):
+                    #dodac menu opcji - odzielny? plik np settignsMenu.py i import klasy
                     print("SETTTINGS")
-                if 685 <= mouse_pos[0] <= 1230 and 445 <= mouse_pos[1] <= 505 :
+                if dummyButton.check_click(mousePos):
                     print("DUMMYBUTTON")
-                if 685 <= mouse_pos[0] <= 1230 and 545 <= mouse_pos[1] <= 605:
+                if quitButton.check_click(mousePos):
                     pygame.quit()
                     sys.exit()
+            #hujowe - do przerobki odczytwyanie klikniecia
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     mouse_pos = pygame.mouse.get_pos()
+            #     #print(mouse_pos)
+            #     if 685 <= mouse_pos[0] <= 1230 and 245 <= mouse_pos[1] <= 305:
+            #         main_game()
+            #     if 685 <= mouse_pos[0] <= 1230 and 345 <= mouse_pos[1] <= 405:
+            #         print("SETTTINGS")
+            #     if 685 <= mouse_pos[0] <= 1230 and 445 <= mouse_pos[1] <= 505 :
+            #         print("DUMMYBUTTON")
+            #     if 685 <= mouse_pos[0] <= 1230 and 545 <= mouse_pos[1] <= 605:
+            #         pygame.quit()
+            #         sys.exit()
 
         pygame.display.update()
         clock.tick(FPS)
+
 
 
 if __name__ == "__main__":
